75a3220023b35623feb63c59858ad8b7eb37dc57
WildBuck <liukun507@163.com>
2018-06-21 12:35:38 +0000

    move user model to models folder

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -9,10 +9,15 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    //指明数据库表的名称
+    protected $table = 'users';
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
+
+     *过滤用户提交的字段，只有包含在概述性中的字段才能被正常更新
      */
     protected $fillable = [
         'name', 'email', 'password',
@@ -22,6 +27,7 @@ class User extends Authenticatable
      * The attributes that should be hidden for arrays.
      *
      * @var array
+     *隐藏敏感信息
      */
     protected $hidden = [
         'password', 'remember_token',
